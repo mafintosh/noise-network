b9782b19eb3be4327edae2771df65de99ff8bb69
Robert Kowalski <rok@kowalski.gd>
2019-06-12 14:39:50 +0200

    add failing connection test
    https://github.com/mafintosh/noise-network/issues/4

--- tests.js
+++ tests.js
@@ -20,3 +20,31 @@ test('destroy', function (assert) {
     client.end(assert.pass)
   })
 })
+
+test('connect', { timeout: 1000 }, function (assert) {
+  assert.plan(3)
+  var server = network.createServer()
+  var client
+
+  server.on('connection', function (encryptedStream) {
+    assert.pass('Connected')
+
+    encryptedStream.pipe(encryptedStream)
+    encryptedStream.on('error', assert.error)
+
+    encryptedStream.on('data', function (data) {
+      assert.pass('received data')
+
+      server.close()
+      client.end(assert.pass)
+    })
+  })
+
+  var serverKeys = network.keygen()
+  server.listen(serverKeys, connectClient)
+
+  function connectClient () {
+    client = network.connect(serverKeys.publicKey)
+    client.write('hello')
+  }
+})
